0586345b8ddcc0f38460f391a1973c89f52761c1
Serhii Oliinykov <oliinykov@serhiis-macbook-pro.home>
2023-09-06 15:46:36 +0200

    first commit

---
+++ src/components/InfinityLoader/InfinityLoader.tsx
@@ -0,0 +1,81 @@
+import React, {useCallback, useEffect, useRef, useState} from "react"
+import './InfinityLoader.css';
+
+interface IInfinityLoaderProps<T> {
+    fetchData: (page: number, perPage: number) => Promise<T[]>
+    renderItem: (item: T) => React.ReactNode
+    initialPage?: number
+    perPage?: number
+    scrollThreshold?: number
+    className?: string
+}
+
+const PER_PAGE = 10
+
+export const InfinityLoader = <T, >({
+                                        fetchData,
+                                        renderItem,
+                                        initialPage = 1,
+                                        perPage = PER_PAGE,
+                                        scrollThreshold = 0,
+                                        className,
+                                    }: IInfinityLoaderProps<T>) => {
+    const loaderRef = useRef<HTMLDivElement | null>(null)
+    const [items, setItems] = useState<T[]>([])
+    const [page, setPage] = useState(initialPage)
+    const [isLoading, setIsLoading] = useState(false)
+    const [error, setError] = useState<string | null>(null)
+    const loadMore = useCallback(async () => {
+        setIsLoading(true)
+
+        try {
+            const newItems: T[] = await fetchData(page, perPage)
+
+            setItems((prev: T[]) => [...prev, ...newItems])
+            setPage((prev: number) => prev + 1)
+        } catch (error) {
+            setError('Something went wrong')
+        } finally {
+            setIsLoading(false)
+        }
+
+    }, [page, perPage, fetchData])
+
+    const handleObserver = useCallback(async (entries: IntersectionObserverEntry[]) => {
+            if (isLoading || error) { // error doc. try again button
+                return
+            }
+
+            const [target] = entries
+
+            if (target?.isIntersecting) {
+                await loadMore()
+            }
+        },
+        [loadMore, isLoading, error],
+    )
+
+    useEffect(() => {
+        const options = {
+            rootMargin: `${scrollThreshold}px`,
+        }
+
+        const observer = new IntersectionObserver(handleObserver, options)
+
+        if (loaderRef.current) {
+            observer.observe(loaderRef.current)
+        }
+
+        return () => observer.disconnect()
+    }, [handleObserver, scrollThreshold])
+
+    return <section className={className}>
+        {items.map(item => renderItem(item))}
+        {error && <div className="error-message">Error: {error}</div>}
+        {!error && <div ref={loaderRef}>
+            <div className="lds-circle">
+                <div></div>
+            </div>
+        </div>}
+    </section>
+}
